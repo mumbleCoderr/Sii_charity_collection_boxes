71a2a3d74e51db38ada1159083dbd8cfe5300a70
pragma once <mat02biernat@gmail.com>
2025-05-19 11:59:55 +0200

    initialization

---
+++ src/main/java/org/example/sii_charity_collection_boxes/SiiCharityCollectionBoxesApplication.java
@@ -0,0 +1,13 @@
+package org.example.sii_charity_collection_boxes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiiCharityCollectionBoxesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiiCharityCollectionBoxesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/sii_charity_collection_boxes/SiiCharityCollectionBoxesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sii_charity_collection_boxes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiiCharityCollectionBoxesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
